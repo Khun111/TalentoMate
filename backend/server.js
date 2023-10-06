ae6f7bda86fa5084ab435bbb9a68c251f4e638da
Oluwatobi Amure <106190886+Khun111@users.noreply.github.com>
2023-10-06 08:10:45 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 /**
  * Start server
  */
-import express from "express";
+import express from 'express';
+import cors from 'cors'
 //import authRouter from "./authRoutes"
 
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 const port = process.env.PORT || 5000
 
 
-app.use(authRouter);
+//app.use(authRouter);
 
 app.listen(port, () => {console.log('listening on', port)});
